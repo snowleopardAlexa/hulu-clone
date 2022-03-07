4bedd6696a87c9e0cb3cc2bc9ee923eadc4ba8df
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-07 13:39:09 -0500

    change tailwind config

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
    <div className="app">
-
+     hello 
    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
 module.exports = {
-  mode: 'jit',
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*,{js,ts,jsx,tsx}"],
-  content: [],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
