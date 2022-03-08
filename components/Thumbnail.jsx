58ff5f07c54a8ef719990e68e060d5d32b25d0b3
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-07 19:22:40 -0500

    add image thumbnail from url

--- components/Thumbnail.jsx
+++ components/Thumbnail.jsx
@@ -1,6 +1,23 @@
+/* eslint-disable jsx-a11y/alt-text */
+import Image from "next/image";
+
 const Thumbnail = ({ result }) => {
+  
+  const BASE_URL = "https://image.tmdb.org/t/p/original/";  
+
   return (
-    <div>Thumbnail</div>
+    <div>
+        <Image 
+          layout='responsive'
+          src={
+           `${BASE_URL}${result.backdrop_path || 
+              result.poster_path}` ||
+           `${BASE_URL}${result.poster_path}`   
+          }
+          height={1080}
+          width={1920}
+        />
+    </div>
   )
 }
 
