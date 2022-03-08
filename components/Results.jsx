1f4b5734992f820648e036ba8f66a157fec70164
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-07 19:45:38 -0500

    add breakpoints to tailwind.config.js

--- components/Results.jsx
+++ components/Results.jsx
@@ -2,7 +2,7 @@ import Thumbnail from "./Thumbnail";
 
 const Results = ({ results }) => {
   return (
-   <div className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3">
+   <div className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 ">
      {results.map((result) => (
        <Thumbnail key={result.id} result={result} />
      ))}
